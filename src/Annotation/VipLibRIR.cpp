c2d074dc9ff61efc6861ccde0644a378f9c715da
ORTEGA Fabien <Fabien.ORTEGA@cea.fr>
2024-05-24 15:40:58 +0200

    Update LibRIR

--- src/Annotation/VipLibRIR.cpp
+++ src/Annotation/VipLibRIR.cpp
@@ -1,7 +1,7 @@
 /**
  * BSD 3-Clause License
  *
- * Copyright (c) 2023, Institute for Magnetic Fusion Research - CEA/IRFM/GP3 Victor Moncada, L�o Dubus, Erwan Grelier
+ * Copyright (c) 2023, Institute for Magnetic Fusion Research - CEA/IRFM/GP3 Victor Moncada, Léo Dubus, Erwan Grelier
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
