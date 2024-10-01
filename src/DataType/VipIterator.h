4ba3ba5fd239281b91c57aed0d8aea0750993703
Victor Moncada <VM213788@intra.cea.fr>
2024-10-01 09:24:12 +0200

    gcc correction

--- src/DataType/VipIterator.h
+++ src/DataType/VipIterator.h
@@ -71,10 +71,10 @@ namespace Vip
 	///
 	/// Consider the following 2*3 matrix:
 	///
-	/// 	\f[
+	/// \f[
 	///	A = \begin{bmatrix}
-	///	1 & 2   \\
-	///	3 & 4   \\
+	///	1 & 2
+	///	3 & 4
 	///	5 & 6
 	///	\end{bmatrix}.
 	///	\f]
